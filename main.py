02f38247ff333e9d97970f77da91a46c8376a801
Hancheng-Guo <silence6v6@gmail.com>
2025-11-25 15:44:53 +0800

    make plt plot single line while test model

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from src.runner.run_ppo import ppo_train, ppo_test
 
 if __name__ == "__main__":
 
-    # model_name = "2025-11-25_12-48-45"
-    # model_name = ppo_test(model_name="2025-11-23_18-30-10")
+    # model_name = "2025-11-25_13-57-54"
+    # model_name = ppo_test(model_name=model_name)
 
     demo = False
-    base_model_name = "2025-11-25_12-48-45"
+    # demo = True
+    base_model_name = "2025-11-25_13-57-54"
     model_name = ppo_train(base_model_name=base_model_name, demo=demo)
     model_name = ppo_test(model_name=model_name)
 
--- src/runner/run_ppo.py
+++ src/runner/run_ppo.py
@@ -42,7 +42,7 @@ def ppo_train(base_model_name=None, demo=False):
     display_model(train_env)
     
     if demo:
-        demo_env  = make_env("demo")
+        demo_env  = make_env("demo", demo_type="multiple")
         model.learn(total_timesteps=CONFIG["algorithm"]["total_timesteps"],
                     tb_log_name=model_name,
                     callback=RenderCallback(demo_env),
@@ -64,44 +64,46 @@ def ppo_train(base_model_name=None, demo=False):
     return model_name
 
 
-def ppo_test(model_name=None, max_steps=1000):
+def ppo_test(model_name=None, n_tests=3, max_steps=1000):
 
     if model_name:
-        env  = make_env("demo")
+        env = make_env("demo", demo_type="single")
         model = PPO.load(CONFIG["path"]["checkpoints"] + model_name,
                          env=env,
                          )
         obs, info = env.reset()
 
         world_dt = env.env.env.env.dt *env.env.env.env.frame_skip
-        plt_frames = []
-        mjc_frames = []
-        for _ in range(max_steps):
-            action, _ = model.predict(obs, deterministic=True)
-            obs, reward, terminated, truncated, info = env.step(action)
-
-            plt_fig = plt.gcf()
-            buffer = io.BytesIO()
-            plt_fig.canvas.print_png(buffer)
-            buffer.write(buffer.getvalue())
-            plt_img = Image.open(buffer)
-            plt_frames.append(plt_img)
-            
-            mjc_fig = env.env.env.env.render("rgb_array")
-            mjc_frames.append(mjc_fig)
-
-            if terminated or truncated:
-                obs, info = env.reset()
-                break
-
         filepath = CONFIG["path"]["demo"] + model_name.split('.')[0] + "/"
         if not os.path.exists(filepath):
             os.makedirs(filepath)
         _, plt_index = get_next_filename(filepath, "plt_", ".gif")
         _, mjc_index = get_next_filename(filepath, "mjc_", ".gif")
-        
-        imageio.mimsave(filepath + "plt_%d.gif" % (1 + max(plt_index, mjc_index)), plt_frames, fps=1/world_dt)
-        imageio.mimsave(filepath + "mjc_%d.gif" % (1 + max(plt_index, mjc_index)), mjc_frames, fps=1/world_dt)
+        target_index = max(plt_index, mjc_index) + 1
+        for i in range(n_tests):
+            plt_frames = []
+            mjc_frames = []
+            for _ in range(max_steps):
+                action, _ = model.predict(obs, deterministic=True)
+                obs, reward, terminated, truncated, info = env.step(action)
+
+                plt_fig = plt.gcf()
+                buffer = io.BytesIO()
+                plt_fig.canvas.print_png(buffer)
+                buffer.write(buffer.getvalue())
+                plt_img = Image.open(buffer)
+                plt_frames.append(plt_img)
+                
+                mjc_fig = env.env.env.env.render("rgb_array")
+                mjc_frames.append(mjc_fig)
+
+                if terminated or truncated:
+                    imageio.mimsave(filepath + "plt_%d.gif" % target_index, plt_frames, fps=1/world_dt)
+                    imageio.mimsave(filepath + "mjc_%d.gif" % target_index, mjc_frames, fps=1/world_dt)
+                    target_index += 1
+                    obs, info = env.reset()
+                    # break
+                    
         env.close()
         plt.close('all')
 
