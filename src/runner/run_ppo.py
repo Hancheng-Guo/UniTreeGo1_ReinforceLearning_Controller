9a452526a2e10a5cbd5c38280d165db1dbc877b8
Hancheng-Guo <silence6v6@gmail.com>
2025-11-26 20:33:09 +0800

    fix bug of tensorboard exit

--- src/runner/run_ppo.py
+++ src/runner/run_ppo.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from PIL import Image
 
 from src.config.config import CONFIG, update_CONFIG, save_CONFIG
-from src.render.render_tensorboard import init_tensorboard
+from src.render.render_tensorboard import ThreadTensorBoard
 from src.env.make_env import make_env
 from src.env.display_model import display_model
 from src.env.callbacks import RenderCallback
@@ -19,8 +19,9 @@ from src.utils.update_checkpoints_tree import update_checkpoints_tree
 
 def ppo_train(base_model_name=None, demo=False):
 
-    note = input("\nPlease enter the notes for the current training model:\n")
-    init_tensorboard()
+    note = input("\nPlease enter the notes for the current training model:\n > ")
+    tensorboard_thread = ThreadTensorBoard()
+    tensorboard_thread.run()
 
     model_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     train_env = make_env("train")
@@ -59,6 +60,7 @@ def ppo_train(base_model_name=None, demo=False):
     shutil.copy2(CONFIG["path"]["env_class_py"], CONFIG["path"]["env_backup"] + model_name + ".py")
     save_CONFIG(CONFIG["path"]["config_backup"] + model_name + ".yaml")
     
+    tensorboard_thread.stop()
     train_env.close()
     plt.close('all')
 
