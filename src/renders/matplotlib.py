bd66549796e5e2d2a5ce1858a682a155cfbc257e
Hancheng-Guo <silence6v6@gmail.com>
2025-12-14 10:30:27 +0800

    optimize structure of project

--- src/renders/matplotlib.py
+++ src/renders/matplotlib.py
@@ -1,12 +1,7 @@
 import math
-import matplotlib
-import time
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-from mpl_toolkits.mplot3d import Axes3D
-from src.utils.noop import noop
-from src.config.config import CONFIG
 
 
 def plt_select_kwargs(state, info):
@@ -31,7 +26,9 @@ class PltRenderer():
                  title_size=8.4,
                  label_size=8.4,
                  xlabel_size=7,
-                 ylabel_size=7,
+                 ylabel_size: float = 7.,
+                 plt_n_lines: int = 1,
+                 plt_x_range: int = 200,
                  ):
         # set plt
         self.render_mode = render_mode
@@ -42,21 +39,21 @@ class PltRenderer():
         plt.rcParams["ytick.labelsize"] = ylabel_size
         if render_mode == "human":
             plt.ion()
-        # set_fig
+        # set fig and axes
         selected_kwargs = plt_select_kwargs([],{})
         self.subfigs = len(selected_kwargs)
         self.cols = min(plt_max_col, self.subfigs)
         self.rows = math.ceil(self.subfigs / self.cols)
         self.fig, self.plt_axes = plt.subplots(self.rows, self.cols, figsize=(1.25*self.cols, 1.25*self.rows)) # default figsize=(6.4, 4.8)
         self.plt_axes = self.plt_axes.flatten()
+        # set plt_line and plt_data
+        self.plt_n_lines = plt_n_lines
+        self.plt_x_range = plt_x_range
         for i, (key, _) in enumerate(selected_kwargs.items()):
             self.plt_axes[i].set_title(key)
-        self.plt_line = [[] for _ in range(CONFIG["demo"]["plt_n_lines"])]
-        self.plt_data = dict()
-        for i, (key, _) in enumerate(selected_kwargs.items()):
-            self.plt_data[key] = list()
-            line, = self.plt_axes[i].plot([], []) 
-            self.plt_line[0].append(line)
+        self.plt_line = [[] for _ in range(self.plt_n_lines)]
+        self._init()
+        print()
         
     def __call__(self, state, info, *args, **kwds):
         selected_kwargs = plt_select_kwargs(state, info)
@@ -66,7 +63,6 @@ class PltRenderer():
         return Image.fromarray(plt_img, mode="RGBA")
     
     def reset(self):
-        self.plt_data = dict()
         for line in self.plt_line[0]:
             line.set_alpha(1)
         for line in self.plt_line[len(self.plt_line) - 1]:
@@ -74,25 +70,39 @@ class PltRenderer():
         for i in range(len(self.plt_line) - 1, 0, -1):
             self.plt_line[i] = self.plt_line[i - 1]
             for line in self.plt_line[i]:
-                line.set_alpha(line.get_alpha() - 1 / self.plt_n_lines)
+                line.set_alpha(line.get_alpha() - 1 / len(self.plt_line))
+        self._init()
+
+    def _init(self):
+        self.plt_data = dict()
         self.plt_line[0] = list()
+        for i, (key, _) in enumerate(plt_select_kwargs([],{}).items()):
+            self.plt_data[key] = list()
+            line, = self.plt_axes[i].plot([], []) 
+            self.plt_line[0].append(line)
+
 
     def _plot(self, selected_kwargs):
         for i, (key, value) in enumerate(selected_kwargs.items()):
             self.plt_data[key].append(value["value"])
             line_data = np.unwrap(self.plt_data[key]) if value["needs_unwrap"] else self.plt_data[key]
-            self.plt_line[0][i].set_data(range(len(line_data)), line_data) 
-            self._update_lim(self.plt_axes[i])
+            self.plt_line[0][i].set_data(range(len(line_data)), line_data)
+            if len(line_data):
+                self._update_lim(self.plt_axes[i])
         plt.tight_layout()
         if self.render_mode == "human":
             plt.pause(0.00001)
 
     def _update_lim(self, ax):
-        ax.relim()
-        ax.set_xlim(max(ax.dataLim.x0, ax.dataLim.x1 - CONFIG["demo"]["plt_x_range"]),
-                    max(ax.dataLim.x0 + CONFIG["demo"]["plt_x_range"], ax.dataLim.x1))
-        if ax.dataLim.y1 - ax.dataLim.y0 < 0.5:
-            y_padding = (0.5 - (ax.dataLim.y1 - ax.dataLim.y0)) / 2
-        else:
-            y_padding = 0
-        ax.set_ylim(ax.dataLim.y0 - y_padding, ax.dataLim.y1 + y_padding)
+        try:
+            ax.relim()
+            ax.set_xlim(max(ax.dataLim.x0, ax.dataLim.x1 - self.plt_x_range),
+                        max(ax.dataLim.x0 + self.plt_x_range, ax.dataLim.x1))
+            if ax.dataLim.y1 - ax.dataLim.y0 < 0.5:
+                y_padding = (0.5 - (ax.dataLim.y1 - ax.dataLim.y0)) / 2
+            else:
+                y_padding = 0
+            ax.set_ylim(ax.dataLim.y0 - y_padding, ax.dataLim.y1 + y_padding)
+        except:
+            pass
+        
