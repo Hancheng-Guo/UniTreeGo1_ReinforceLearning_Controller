ca8c5e2661048d04e36202e87b03d090d2fa2c6c
Hancheng-Guo <silence6v6@gmail.com>
2026-01-15 22:51:02 +0800

    hierarchical_ppo struct construct

--- src/runner/base.py
+++ src/runner/base.py
@@ -1,8 +1,10 @@
 from src.runner.flat_ppo_runner import FlatPPORunner
+from src.runner.mode_hierarchical_ppo_runner import ModeHierarchicalPPORunner
 
 
 __all__ = [
     "FlatPPORunner",
+    "ModeHierarchicalPPORunner",
     ]
 
 
@@ -11,3 +13,7 @@ from gymnasium.envs.registration import register
 
 register(id="FlatLocomotionEnv",
          entry_point="src.env.flat_locomotion_env:FlatLocomotionEnv")
+register(id="ModeConditionedLocomotionEnv",
+         entry_point="src.env.mode_conditioned_locomotion_env:ModeConditionedLocomotionEnv")
+register(id="ModeSelectionEnv",
+         entry_point="src.env.mode_selection_env:ModeSelectionEnv")
---
+++ src/runner/mode_hierarchical_ppo_runner.py
@@ -0,0 +1,320 @@
+import os
+import numpy as np
+import yaml
+import time
+from torch import nn
+import matplotlib.pyplot as plt
+from src.callback.base import Stage
+from src.callback.base import CustomCheckpointCallback
+from src.callback.base import AdaptiveLRCallback
+from src.callback.base import TrainProgressCallback, TestProgressCallback, IterProgressCallback
+from src.callback.base import CustomTensorboardCallback
+from src.callback.base import StageScheduleCallback
+from src.callback.base import RenderSaverCallback
+from src.config.base import update_config, get_config
+from stable_baselines3.common.env_util import make_vec_env
+from src.runner.common.ppo_runner import PPOTrainer, PPOTester
+from src.callback.base import IterCallBackList
+from stable_baselines3.common.vec_env import VecNormalize
+
+class ModeHierarchicalPPORunner(PPOTrainer, PPOTester):
+    def __init__(self,
+                 base_name: str = None):
+        with open("./src/config/hierarchical_ppo_config.yaml", "r") as f:
+           config = yaml.safe_load(f)
+        super().__init__(config, base_name)
+
+    def train(self,
+              config_inheritance: bool = False,
+              note_skip: bool = False,
+              tensorboard_skip : bool = False):
+        
+        self.get_note(note_skip)    # Get training note information
+        self.get_save_name()        # Get save name and directory
+        self.run_tensorboard(tensorboard_skip)      # Start TensorBoard thread for logging
+        self.inherite_config(config_inheritance)    # Inherit config
+        self.get_algorithm_kwargs()                 # Prepare algorithm parameters
+        self.make_loco_train_env("ModeConditionedLocomotionEnv")    # Create parallel locomotion training environment
+        self.make_mode_train_env("ModeSelectionEnv")        # Create parallel mode training environment
+        self.load_model_with_train_env(tensorboard_skip)    # Load model and environment
+
+        self.get_callback_kwargs()  # Get callback function parameters
+        self.get_training_kwargs()  # Get training parameters
+        # Start model training process
+        self.learn(loco_callback=[IterProgressCallback(**self.loco_callback_kwargs),
+                                  CustomTensorboardCallback(**self.loco_callback_kwargs),
+                                  AdaptiveLRCallback(**self.loco_callback_kwargs)],
+                   mode_callback=[IterProgressCallback(**self.mode_callback_kwargs),
+                                  CustomTensorboardCallback(**self.mode_callback_kwargs),
+                                  AdaptiveLRCallback(**self.mode_callback_kwargs)])
+        self.train_env_close()  # Clean up resources
+
+    def learn(self,
+              com_callback: list = [],
+              loco_callback: list = [],
+              mode_callback: list = [],):
+
+        self.loco_total_timesteps, _ = self.loco_model._setup_learn(**self.loco_training_kwargs)
+        self.mode_total_timesteps, _ = self.mode_model._setup_learn(**self.mode_training_kwargs)
+        
+        self.loco_callback = IterCallBackList([*com_callback,*loco_callback])
+        self.loco_callback.init_callback(self.loco_model)
+        self.mode_callback = IterCallBackList([*com_callback,*mode_callback])
+        self.mode_callback.init_callback(self.mode_model)
+
+        self.loco_callback.on_training_start(locals(), globals())
+        self.mode_callback.on_training_start(locals(), globals())
+
+        assert self.loco_model.env is not None and self.mode_model.env is not None
+
+        iteration = 0
+        loco_rollout_count = 0
+        mode_rollout_count = 0
+        while iteration < self.total_iterations:
+            self.loco_callback.on_iteration_start()
+            self.mode_callback.on_iteration_start()
+
+            tmp = None
+            loco_collect_time = 0
+            loco_total_time = None
+            mode_collect_time = 0
+            mode_total_time = None
+
+            loco_total_time = time.perf_counter()
+            while True:
+                tmp = time.perf_counter()
+                continue_training = self.loco_model.collect_rollouts(env=self.loco_model.env,
+                                                                     callback=self.loco_callback,
+                                                                     rollout_buffer=self.loco_model.rollout_buffer,
+                                                                     n_rollout_steps=self.loco_model.n_steps)
+                loco_collect_time += time.perf_counter() - tmp
+                loco_rollout_count += 1
+                if not continue_training:
+                    iteration = self.total_iterations
+                    break
+
+                self.loco_model._update_current_progress_remaining(self.loco_model.num_timesteps, self.loco_total_timesteps)
+
+                assert self.loco_model.ep_info_buffer is not None
+                self.loco_model.dump_logs(loco_rollout_count)
+                self.loco_model.train()
+
+                if self.loco_model.num_timesteps % self.loco_iteration_steps == 0:
+                    break
+            loco_total_time = time.perf_counter() - loco_total_time
+
+            mode_total_time = time.perf_counter()
+            while True:
+                tmp = time.perf_counter()
+                continue_training = self.mode_model.collect_rollouts(env=self.mode_model.env,
+                                                                     callback=self.mode_callback,
+                                                                     rollout_buffer=self.mode_model.rollout_buffer,
+                                                                     n_rollout_steps=self.mode_model.n_steps)
+                mode_collect_time += time.perf_counter() - tmp
+                mode_rollout_count += 1
+                if not continue_training:
+                    iteration = self.total_iterations
+                    break
+
+                self.mode_model._update_current_progress_remaining(self.mode_model.num_timesteps, self.mode_total_timesteps)
+
+                assert self.mode_model.ep_info_buffer is not None
+                self.mode_model.dump_logs(mode_rollout_count)
+                self.mode_model.train()
+
+                if self.mode_model.num_timesteps % self.mode_iteration_steps == 0:
+                    break
+            mode_total_time = time.perf_counter() - mode_total_time
+
+
+            print(f"loco: {(loco_collect_time / loco_total_time * 100):.2f}%")
+            print(f"mode: {(mode_collect_time / mode_total_time * 100):.2f}%")
+
+            iteration += 1
+            self.loco_callback.on_iteration_end()
+            self.mode_callback.on_iteration_end()
+
+
+        self.loco_callback.on_training_end()
+        self.mode_callback.on_training_end(locals(), globals())
+
+
+    def get_algorithm_kwargs(self):
+        """Extract and prepare algorithm hyperparameters from configuration for RL algorithm initialization.
+        
+        This function extracts algorithm-related parameters like steps, batch size, etc.
+        It also processes the activation function in the policy network by 
+        converting string representations to actual neural network activation functions.
+
+        :Updates:
+        - :self.algorithm_kwargs: get algorithm hyperparameters including n_steps, batch_size, learning_rate, etc.
+        - :self.loco_algorithm_kwargs: algorithm kwargs for locomotion policy including activation function mapping
+        - :self.mode_algorithm_kwargs: algorithm kwargs for mode policy including activation function mapping
+        """
+        # Extract algorithm-related parameters from the configuration
+        self.algorithm_kwargs = get_config(config=self.config, field="algorithm", try_keys=["device", "verbose"])
+
+        self.loco_algorithm_kwargs = get_config(config=self.config, field="algorithm", try_keys=["loco"])["loco"]
+        activation_fn = self.loco_algorithm_kwargs["policy_kwargs"].get("activation_fn", "")
+        self.loco_algorithm_kwargs["policy_kwargs"]["activation_fn"] = nn.ELU if activation_fn == "ELU" else nn.Tanh
+
+        self.mode_algorithm_kwargs = get_config(config=self.config, field="algorithm", try_keys=["mode"])["mode"]
+        activation_fn = self.mode_algorithm_kwargs["policy_kwargs"].get("activation_fn", "")
+        self.mode_algorithm_kwargs["policy_kwargs"]["activation_fn"] = nn.ELU if activation_fn == "ELU" else nn.Tanh
+
+    def get_training_kwargs(self):
+        self.total_iterations = self.config["train"]["total_iterations"]
+        self.mode_iteration_steps = self.config["train"]["mode_iteration_steps"]
+        self.loco_iteration_steps = self.config["train"]["loco_iteration_steps"]
+        self.mode_training_kwargs = {
+            "total_timesteps": self.config["train"]["mode_iteration_steps"] * self.config["train"]["total_iterations"],
+            "reset_num_timesteps": False,
+            "tb_log_name": f"log_{self.save_name}_mode",
+        }
+        self.loco_training_kwargs = {
+            "total_timesteps": self.config["train"]["loco_iteration_steps"] * self.config["train"]["total_iterations"],
+            "reset_num_timesteps": False,
+            "tb_log_name": f"log_{self.save_name}_loco",
+        }
+
+    def get_callback_kwargs(self):
+        """
+        Creates and returns a dictionary of keyword arguments for initializing various callbacks
+        used during the PPO training process.
+
+        :Updates:
+        - :self.callback_kwargs: A dictionary containing keyword arguments for various training callbacks
+        """
+        try:
+            # Attempt to load the base stage from .npy file
+            base_stage_path = os.path.join(self.base_dir, f"cst_{self.base_name}.npy")
+            base_stage = np.load(base_stage_path)
+        except:
+            # Default to idle stage if file doesn't exist or can't be loaded
+            base_stage = Stage.idle
+        
+        loco_rollout_steps = self.loco_model.n_steps * self.loco_model.n_envs
+        loco_iteration_steps = self.config["train"]["loco_iteration_steps"]
+        loco_iteration_rollouts = (loco_iteration_steps + loco_rollout_steps - 1) // loco_rollout_steps
+
+
+        mode_rollout_steps = self.mode_model.n_steps * self.mode_model.n_envs
+        mode_iteration_steps = self.config["train"]["mode_iteration_steps"]
+        mode_iteration_rollouts = (mode_iteration_steps + mode_rollout_steps - 1) // mode_rollout_steps
+
+        # n_iterations = (self.config["train"]["total_timesteps"] + iteration_steps - 1) // iteration_steps
+        
+
+        self.com_callback_kwargs = {
+            # for StageScheduleCallback
+            "base_stage": base_stage,
+            # for CustomCheckpointCallback
+            "note": self.note,
+            "config": self.config,
+            "save_name": self.save_name,
+            "save_dir": self.save_dir,
+            "save_freq": self.config["train"]["checkpoint_freq"],
+            "loco_env_py_path": self.config["path"]["loco_env_py"],
+            "mode_env_py_path": self.config["path"]["mode_env_py"],
+            "checkpoints_path": self.config["path"]["output"],
+            "base_name": self.base_name
+        }
+
+        self.loco_callback_kwargs = {
+            # for ProgressBar
+            "rollout_steps": loco_rollout_steps,
+            "custom_str": "Loco-Rollout",
+            "n_rollouts": self.config["train"]["total_iterations"] * loco_iteration_rollouts,
+            # for CustomTensorboardCallback
+            "log_freq": self.config["train"]["custom_loco_log_freq"],
+            "tensorboard_items": self.config["tensorboard_items"]["loco"],
+            # for AdaptiveLRCallback
+            "init_lr": self.config["algorithm"]["loco"]["learning_rate"],
+        }
+
+        self.mode_callback_kwargs = {
+            # for ProgressBar
+            "rollout_steps": mode_rollout_steps,
+            "custom_str": "Mode-Rollout",
+            "n_rollouts": self.config["train"]["total_iterations"] * mode_iteration_rollouts,
+            # for CustomTensorboardCallback
+            "log_freq": self.config["train"]["custom_mode_log_freq"],
+            "tensorboard_items": self.config["tensorboard_items"]["mode"],
+            # for AdaptiveLRCallback
+            "init_lr": self.config["algorithm"]["mode"]["learning_rate"],
+        }
+
+      
+    def make_loco_train_env(self, env_name):
+        self.make_train_env(env_name)
+        self.loco_train_env = self.train_env
+
+    def make_mode_train_env(self, env_name):
+        self.make_train_env(env_name)
+        self.mode_train_env = self.train_env
+
+    def load_model_with_train_env(self,
+                                  tensorboard_skip: bool = False,
+                                  **kwargs):
+        kwargs.update(tensorboard_log=None if tensorboard_skip else self.save_dir)
+        self.mode_model, self.mode_train_env = super().load_model(self.mode_train_env,
+                                                                  {**self.algorithm_kwargs, **self.mode_algorithm_kwargs},
+                                                                  **kwargs)
+        self.loco_model, self.loco_train_env = super().load_model(self.loco_train_env,
+                                                                  {**self.algorithm_kwargs, **self.loco_algorithm_kwargs},
+                                                                  **kwargs)
+        self.display_train_env(self.loco_train_env.venv.envs[0].env)
+        
+        for env in self.loco_train_env.venv.envs:
+            env.env.env.env.env.mode_model = self.mode_model
+        
+        for env in self.mode_train_env.venv.envs:
+            loco_env = self.make_gym_env("ModeConditionedLocomotionEnv")
+            env.env.env.env.env.loco_env = loco_env
+            env.env.env.env.env.loco_normalize_obs = self.loco_train_env.normalize_obs
+            env.env.env.env.env.loco_discontruct_obs = loco_env.env.env.env._discontruct_obs
+            env.env.env.env.env.loco_model = self.loco_model
+        
+            
+
+    def train_env_close(self):
+        if self.tensorboard_thread:
+            self.tensorboard_thread.stop()
+        self.test_env.close()
+        self.loco_train_env.close()
+        self.mode_train_env.close()
+        plt.close('all')
+
+        print(f"\nModel {self.save_name} training accomplished!\n")
+
+        self.base_name = self.save_name
+        self.check_base_name()
+
+    def test(self, n_tests=3, max_steps=1000):
+        
+        if self.base_name:
+            self.make_test_env("FlatLocomotionEnv") # Create vectorized environment for testing
+            self.load_model_with_test_env()         # Load pre-trained model and environment
+            self.register_callbacks([TestProgressCallback(n_tests, max_steps),
+                                     RenderSaverCallback(self)])
+
+            for i in range(n_tests):
+                self._dispatch("_on_test_start")
+                obs = self.test_env.reset()
+                for j in range(max_steps):
+                    action, _ = self.model.predict(obs, deterministic=True)
+                    obs, reward, done, info = self.test_env.step(action)
+                    if done:
+                        break
+                    self._dispatch("_on_test_step")
+                self._dispatch("_on_test_end")
+                
+            self.test_env.close()   # Clean up resources
+    
+    def load_model_with_test_env(self, **kwargs):
+        super().load_model_with_test_env(**kwargs)
+        self.test_env.envs[0].env.env.env.env.stage = np.load(os.path.join(self.base_dir,
+                                                                           f"cst_{self.base_name}.npy"))
+
+
+
